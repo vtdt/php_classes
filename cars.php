e307e7424f6c8b6d8338c35d465e029af07cb0a0
vtdt <127406000+vtdt@users.noreply.github.com>
2023-11-24 09:51:51 +0200

    Update cars.php

--- cars.php
+++ cars.php
@@ -3,7 +3,7 @@
 class Car{
     public $brand;
     private $mileage;
-
+    static $describe;
     public function __construct($b, $m){
         $this->brand = $b;
         $this->mileage=$m;
@@ -13,6 +13,10 @@ class Car{
         echo $this->brand . " is dead at mileage " . $this->mileage . "<br>";
     }
     
+
+    public function makeNoise(){
+        echo "Beep, Beep!";
+    }
 
     public function increaseMileage($x){
         $this->mileage += $x;
